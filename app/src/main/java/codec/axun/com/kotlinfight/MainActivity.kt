2b1e9671a0ef23fbe7b1a6250298d2442b10bb39
18869978285 <18869978285@163.com>
2018-03-28 18:27:06 +0800

    瀑布流

--- app/src/main/java/codec/axun/com/kotlinfight/MainActivity.kt
+++ app/src/main/java/codec/axun/com/kotlinfight/MainActivity.kt
@@ -2,28 +2,61 @@ package codec.axun.com.kotlinfight
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import codec.axun.com.kotlinfight.adapter.ShowImageAdapte
 import codec.axun.com.kotlinfight.bean.KeyCenter
 import codec.axun.com.kotlinfight.bean.Picture
 import codec.axun.com.kotlinfight.presenter.IMainPresenter
 import codec.axun.com.kotlinfight.view.IMainView
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), IMainView {
     private var page = 1;
     private var items:ArrayList<Picture> = arrayListOf()
     private var mAdapter:ShowImageAdapte = ShowImageAdapte(this,items)
+    private lateinit var presenter:IMainPresenter
+    private lateinit var toast: Toast
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val presenter = IMainPresenter(this)
+        initRecyclerView()
+        initRefresh()
+        presenter = IMainPresenter(this)
         presenter.getNetData(KeyCenter.url+page)
-//        mAdapter = ShowImageAdapte(this,items)
-        val manager = LinearLayoutManager(this)
+        toast = Toast.makeText(this,"",Toast.LENGTH_SHORT)
+        toast.duration = Toast.LENGTH_SHORT
+
+    }
+
+    private fun initRecyclerView(){
+        val manager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
         rv_show_image.layoutManager = manager
+        rv_show_image.itemAnimator = DefaultItemAnimator()
         rv_show_image.adapter = mAdapter
+        mAdapter.setOnItemClickListener(object :ShowImageAdapte.OnItemClickListener{
+            override fun onItemClick(view: View, position: Int) {
+                toast.setText(items[position].title)
+                toast.show()
+            }
+
+        })
+    }
+
+    private fun initRefresh(){
+        refresh_layout.setEnableRefresh(false)
+        refresh_layout.setEnableLoadmore(true)
+        refresh_layout.setOnRefreshListener(object:RefreshListenerAdapter(){
+            override fun onLoadMore(refreshLayout: TwinklingRefreshLayout?) {
+                presenter.getNetData(KeyCenter.url+page)
+            }
+        })
+
     }
 
 
@@ -40,9 +73,13 @@ class MainActivity : AppCompatActivity(), IMainView {
            Log.d("main",data.size.toString())
            items.addAll(data)
            mAdapter.notifyDataSetChanged()
+           page++
+           refresh_layout.finishLoadmore()
        }
     }
 
 
 
 }
+
+
--- app/src/main/java/codec/axun/com/kotlinfight/adapter/ShowImageAdapter.kt
+++ app/src/main/java/codec/axun/com/kotlinfight/adapter/ShowImageAdapter.kt
@@ -1,12 +1,12 @@
 package codec.axun.com.kotlinfight.adapter
 
 import android.content.Context
-
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import codec.axun.com.kotlinfight.R
 import codec.axun.com.kotlinfight.bean.Picture
 import com.bumptech.glide.Glide
@@ -23,19 +23,42 @@ class ShowImageAdapte(private val context: Context, private val items:List<Pictu
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
         val imageHolder = holder as ImageHolder
-        Glide.with(context).load(items.get(position).url).into(imageHolder.iv_pic)
+        var params = imageHolder.iv_pic.layoutParams;
+        params.width = 800
+        params.height = 480
+        Glide.with(context).load(items[position].url).override(800,480).into(imageHolder.iv_pic)
+        imageHolder.tv_title.text = items[position].title
+        holder.itemView.setOnClickListener {
+            if (listener!=null){
+                listener!!.onItemClick(holder.itemView,position)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-       return if (items==null) 0 else items.size
+       return items.size
     }
 
    inner class ImageHolder(view: View) : RecyclerView.ViewHolder(view) {
        var iv_pic:ImageView
-
+       var tv_title:TextView
        init {
            iv_pic = view.findViewById(R.id.show_image)
+           tv_title = view.findViewById(R.id.tv_title)
        }
 
    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener){
+        this.listener = listener
+    }
+
+
+    var listener:OnItemClickListener?=null
+
+    interface OnItemClickListener{
+        fun onItemClick(view: View,position: Int)
+    }
+
+
 }
